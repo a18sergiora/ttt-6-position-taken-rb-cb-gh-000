ea159b82405decad4f19d559797002929609613d
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2019-08-08 22:48:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
 
 
 def position_taken?(board,index)
-  if board[index]==(" ")
+  if board[index]==(" ") || board[index]==""
     return false
   else
     return true
