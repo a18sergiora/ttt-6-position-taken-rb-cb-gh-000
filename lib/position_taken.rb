ed69d7c561a3f532534c59a465d77518e952a9a9
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2019-08-08 22:45:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
+
 # code your #position_taken? method here!
+board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def position_taken(board,index)
+  if board[index].eq(" ")
+    return false
+  else
+    return true
+  end
+end
+
+position_taken(board,2)
